f362253b078f74c12db5037d5a9e9fa2ceeef112
vaishnavi9542 <vaishuvadla1575@gmail.com>
2025-09-12 21:45:01 +0530

    Create Binary_search.py

---
+++ Binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(arr, val):
+    low = 0
+    high = len(arr) - 1  
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == val:
+            return mid
+        elif arr[mid] < val:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+arr = list(map(int, input("Enter sorted array: ").split()))
+val = int(input("Enter value to search: "))
+result = binary_search(arr, val)
+if result != -1:
+    print("Element found at index {result}")
+else:
+    print("Element not found")
